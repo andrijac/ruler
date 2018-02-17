aed4f74aced3ccdfeef9e357ef1065d86ca6c4a4
Andrija <andrija11@gmail.com>
2018-02-17 13:31:40 +0100

    Added Unit Test project.

---
+++ Source/Ruler.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ruler.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ruler.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6eec3011-3871-49ac-ba14-87e3ea3e2d84")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Source/Ruler.Tests/RulerInfoTests.cs
@@ -0,0 +1,51 @@
+﻿using Ruler;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ruler.Test
+{
+	[TestClass]
+	public class RulerInfoTests
+	{
+		[TestMethod]
+		public void GetDefaultRulerInfoTest()
+		{
+			Assert.AreNotEqual(null, RulerInfo.GetDefaultRulerInfo());
+		}
+
+		[TestMethod]
+		public void CopyIntoTest()
+		{
+			IRulerInfo source = new RulerInfo
+			{
+				Width = 500,
+				Height = 80,
+				Opacity = 0.90,
+				ShowToolTip = true,
+				IsLocked = true,
+				IsVertical = true,
+				TopMost = true
+			};
+
+			IRulerInfo target = new RulerInfo
+			{
+				Width = 400,
+				Height = 75,
+				Opacity = 0.60,
+				ShowToolTip = true,
+				IsLocked = false,
+				IsVertical = false,
+				TopMost = true
+			};
+
+			RulerInfo.CopyInto(source, target);
+
+			var properties = Helper.GetPublicPropertiesFromInterface(typeof(IRulerInfo));
+
+			foreach (PropertyInfo pi in properties)
+			{
+				Assert.AreEqual(pi.GetValue(source), pi.GetValue(target));
+			}
+		}
+	}
+}
